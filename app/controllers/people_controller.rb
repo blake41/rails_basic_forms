0ba01b812dde777e26dfed3a1b6692bfb14395eb
Blake Johnson <blake41@gmail.com>
2015-09-22 16:20:54 -0400

    strong params fixed

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,12 +1,16 @@
 class PeopleController < ApplicationController
 
   def new
-    
+    @person = Person.new
   end
 
   def create
     binding.pry
-    @person = Person.create(params[:person])
+    @person = Person.create(person_params)
+  end
+
+  def person_params
+    params.require(:person).permit(:name, :city_id)
   end
 
 
